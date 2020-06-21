52673c6bbb7606ce6f93acd9ffb5614f4e85428f
wuff1996 <1027309561@qq.com>
2020-06-21 22:36:21 +0800

    快速排序

---
+++ bombo.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+
+func main(){
+
+    arr := []int{4,5,6,7,3,453,5,7,8,33,44}
+    for i,_ := range arr{
+    	for j := 0;j+i<len(arr)-1;j++{
+    		if(arr[j]>arr[j+1]){
+    			arr[j],arr[j+1] = arr[j+1],arr[j]
+    		}
+    	}
+    }
+    fmt.Println(arr)
+
+
+
+}
