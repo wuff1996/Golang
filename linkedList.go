a16e1415d57dee4f3bb3b2914847f4530cfc5e4b
wuff1996 <1027309561@qq.com>
2020-06-22 00:08:36 +0800

    简单的链表

---
+++ linkedList.go
@@ -0,0 +1,27 @@
+package main
+
+
+
+func main(){
+	var node [5]node
+	for i := range node{
+		node[i].data = 10-i
+		if i!=len(node)-1{
+
+			node[i].next = &node[i+1]
+
+		}
+		
+		
+
+	}
+	
+
+
+}
+
+type node struct{
+	data int
+	next *node
+}
+
